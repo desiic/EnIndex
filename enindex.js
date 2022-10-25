5fd3b6f48411ae4e5737688a28f1a0a6304cce49
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-10-25 13:07:30 +0000

    Quick commit

--- enindex.js
+++ enindex.js
@@ -1,11 +1,19 @@
 // Conventions
-// C++ STL style
-//   - my_namespace
-//   - my_class
-//   - my_method
-//   - my_primitive -- Props, vars, all in lowercase as in C++ STL
-//   - My_Compound  -- Props, vars, the only difference from C++ STL
-//   - MY_INSTANCE
+// ===========
+// Naming rules:
+// Rather similar to natural language, only my_primitive is maths
+// Rather similar to C++ STL standard, only My_Compound differs
+//   - my_namespace -- Generic nouns,    eg. app.ui.utils
+//   - my_class     -- Generic nouns,    eg. technical_manager
+//   - my_method    -- Verbs, aux verbs, eg. process_documents
+//   - my_primitive -- Specific nouns,   eg. i, j, k, n (specific nouns, but maths)
+//   - My_Compound  -- Specific nouns,   eg. John_Doe
+//   - MY_INSTANCE  -- Numbers, digits,  eg. MAX_PRICE
+// Other guides:
+//   - Line <80 chars, max 120 chars
+//   - Max 100 lines per function
+//   - Max 100 functions per file
+//   - Try to reduce lines but more comments
 
 // Modules
 import * as base from "./modules/base.js";
