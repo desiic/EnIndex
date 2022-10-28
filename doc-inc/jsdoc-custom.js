c8cc1298aa01d422241163548e36cbbc4c73da49
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-10-29 01:04:06 +0700

    Msg

--- doc-inc/jsdoc-custom.js
+++ doc-inc/jsdoc-custom.js
@@ -13,8 +13,11 @@ function d$$(Selector){
 
 // Mod func
 function mod_func(){
-    var Togs = d$$(".sidebar-section-title");
-    log(Togs)
+    // Expand all sections in left sidebar
+    var Togs = d$$("#sidebar .sidebar-section-title");
+    
+    for (let Tog of Togs)
+        Tog.setAttribute("data-isopen","true");
 }
 
 window.addEventListener("load",mod_func);
