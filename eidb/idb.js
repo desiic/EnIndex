12c140eab1ef4f23720e62b8e866b42bc63e7a38
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-10-29 00:34:27 +0700

    Msg

--- eidb/idb.js
+++ eidb/idb.js
@@ -20,9 +20,9 @@ class idb{
     /**
      * Call open db on default instance of IDBFactory already at `window.indexedDB`.
      * [See here](module-eidb_idb_idb_factory-idb_factory.html#.open)
-     * @param  {String} Name    - See `eidb.idb.idb_factory` class
-     * @param  {Number} version - See `eidb.idb.idb_factory` class
-     * @return {Array}  See `eidb.idb.idb_factory` class
+     * @param  {String} Name    - See `eidb.idb.idb_factory` class [here](module-eidb_idb_idb_factory-idb_factory.html#.open)
+     * @param  {Number} version - See `eidb.idb.idb_factory` class [here](module-eidb_idb_idb_factory-idb_factory.html#.open)
+     * @return {Array}  See `eidb.idb.idb_factory` class [here](module-eidb_idb_idb_factory-idb_factory.html#.open)
      */
     static async open(Name, version){
         return await (new idb_factory(window.indexedDB)).open(Name,version);
