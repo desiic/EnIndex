5dd5bc30a4968d35e5e41ee2cf756c74020abc08
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-10-29 23:22:24 +0700

    Added idb-object-store

--- eidb/idb/idb-database.js
+++ eidb/idb/idb-database.js
@@ -35,6 +35,7 @@ class idb_database {
 
     /**
      * Get name
+     * @return {String}
      */
     get Name(){
         return this.self.name;
@@ -42,6 +43,7 @@ class idb_database {
 
     /**
      * Get objectStoreNames
+     * @return {Array}
      */
     get Object_Store_Names(){
         return [...this.self.objectStoreNames];
@@ -49,9 +51,52 @@ class idb_database {
 
     /**
      * Get version
+     * @return {Number}
      */
     get version(){
         return this.self.version;
+    }
+
+    /**
+     * _________________________________________________________________________
+     */
+    EVENTS;
+
+    /**
+     * Close event, callback receives single param event
+     */
+    set on_close(callback){
+        this.self.onclose = callback;
+    }
+
+    /**
+     * Version change event, callback receives single param event
+     */ 
+    set on_version_change(callback){
+        this.self.onversionchange = callback;
+    }
+
+    /**
+     * _________________________________________________________________________
+     */
+    METHODS;
+
+    /**
+     * Close database connection
+     * @return {null}
+     */
+    close(){
+        return this.self.close();
+    }
+
+    /**
+     * Create object store
+     * @param  {String}           Name    - Name of object store
+     * @param  {Object}           Options - Store options
+     * @return {idb_object_store}
+     */
+    create_object_store(Name,Options=null){
+        return new idb_object_store(this.self.createObjectStore(Name,Options));
     }
 }
 
