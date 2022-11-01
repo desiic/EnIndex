ec6a4b46575e70be7d01c0a13461f3533e25b920
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-11-01 23:34:16 +0700

    More classes

---
+++ src/eidb/idb.js
@@ -0,0 +1,51 @@
+/**
+ * @module eidb/idb
+ */
+// Modules
+import eidb from "../eidb.js";
+import base from "./base.js";
+
+// Shorthands
+var log      = console.log;
+var logw     = console.warn;
+var loge     = console.error;
+var new_lock = base.new_lock;
+
+/** 
+ * `eidb.idb` IndexedDB wrapper
+ */
+class idb{
+
+    /**
+     * Get the list of databases, using the default instance of IDBFactory at `window.indexedDB`
+     * @return {Array} See `eidb.idb.factory` class [here](module-eidb_idb_factory-factory.html#databases)
+     */
+     static async databases(){
+        return await eidb.Factory.databases();
+    }
+
+    /**
+     * Call open db on default instance of IDBFactory already at `window.indexedDB`.
+     * [See here](module-eidb_idb_factory-factory.html#open)
+     * @param  {String} Name    - See `eidb.idb.factory` class [here](module-eidb_idb_factory-factory.html#open)
+     * @param  {Number} version - See `eidb.idb.factory` class [here](module-eidb_idb_factory-factory.html#open)
+     * @return {Array}  See `eidb.idb.factory` class [here](module-eidb_idb_factory-factory.html#.open)
+     */
+    static async open(Name, version){
+        return await eidb.Factory.open(Name,version);
+    }
+
+    /**
+     * Delete database, using the default instance of IDBFactory already at `window.indexedDB`
+     * [See here](module-eidb_idb_factory-factory.html#delete_database)
+     * @param  {String}      Name - See `eidb.idb.factory` class [here](module-eidb_idb_factory-factory.html#delete_database)
+     * @return {Object|null} See `eidb.idb.factory` class [here](module-eidb_idb_factory-factory.html#delete_database)
+     */
+    static async delete_database(Name){
+        return await eidb.Factory.delete_database(Name);
+    }
+}
+
+// Module export
+export default idb;
+// EOF
---
+++ src/eidb/idb/object-store.js
@@ -0,0 +1,113 @@
+/**
+ * @module eidb/idb/object_store
+ */
+// Modules
+import base from "../base.js";
+
+// Shorthands
+var log      = console.log;
+var logw     = console.warn;
+var loge     = console.error;
+var new_lock = base.new_lock;
+
+/** 
+ * `eidb.idb.object_store` IDBObjectStore class wrapper
+ */
+class object_store {
+
+    /**
+     * Properties
+     */
+    self = null;
+
+    /**
+     * Construct with the IDBObjectStore instance underhood
+     * @param {IDBObjectStore} Idb_Object_Store - IDBObjectStore instance to use
+     */
+    constructor(Idb_Object_Store){
+        this.self = Idb_Object_Store;
+    }
+
+    /**
+     * IN ALPHABETIC ORDER MATCHING MOZILLA.ORG
+     * _________________________________________________________________________
+     */
+    SETS_AND_GETS;
+
+    /**
+     * Get autoIncrement
+     * @return {Boolean}
+     */
+    get auto_increment(){
+        return this.self.autoIncrement;
+    }
+
+    /**
+     * Get indexNames
+     * @return {Array}
+     */
+    get Index_Names(){
+        return [...this.self.indexNames];
+    }
+
+    /**
+     * Get keyPath
+     * @return {String|Array}
+     */
+    get Key_Path(){
+        return this.self.keyPath;
+    }
+
+    /**
+     * Get name
+     * @return {Boolean}
+     */
+    get Name(){
+        return this.self.name;
+    }
+
+    /**
+     * Get current transaction
+     * @return {Object}
+     */
+    get Transaction(){
+        return this.self.transaction;
+    }
+
+    /**
+     * IN ALPHABETIC ORDER MATCHING MOZILLA.ORG
+     * _________________________________________________________________________
+     */
+    METHODS;
+
+    /**
+     * Add value (INSERT), EnIndex has id field autoincremented, 
+     * no key will be passed to IDBObjectStore.add
+     * @return {Array} Error|null and added object id
+     */ 
+    async add(Value){
+        try {
+            var Req           = this.self.add(Value);
+            var [Lock,unlock] = new_lock();
+
+            Req.onerror = function(Ev){
+                unlock(["error", Ev.target.error]);
+            };
+            Req.onsuccess = function(Ev){
+                unlock(["okay", Ev.target.result]);
+            };
+            var [Stat,Result] = await Lock;
+
+            // Result
+            if (Stat=="error") return [Result,null];
+            return [null,Result]; // Added object id
+        }
+        catch (Dom_Exception){
+            return [Dom_Exception, null];
+        }
+    }
+}
+
+// Module export
+export default object_store;
+// EOF
