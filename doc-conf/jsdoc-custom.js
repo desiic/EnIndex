723618470cc256e9f6b9c463bd698c1cd3abfc28
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-11-02 20:58:43 +0700

    Msg

--- doc-conf/jsdoc-custom.js
+++ doc-conf/jsdoc-custom.js
@@ -13,6 +13,14 @@ function d$$(Selector){
 
 // Mod func
 function mod_func(){
+    // Move examples to bottom
+    var Check_Id = d$("#classes");
+
+    if (Check_Id!=null){
+        d$(".main-wrapper article >.container-overview").style.order = 1;
+        d$(".main-wrapper article >.container-overview >h3").style.display = "none";
+    }
+
     // Expand all sections in left sidebar
     var Togs = d$$("#sidebar .sidebar-section-title");
     
