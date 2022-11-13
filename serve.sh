1a51b3d84031608acccd9502df7601dc25a48fd0
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-11-13 16:18:19 +0700

    Test serving HTTPS at 8443

--- serve.sh
+++ serve.sh
@@ -6,5 +6,27 @@ if [[ $BASH_SOURCE != $0 ]]; then
     return
 fi
 
-python3 -m http.server 8080
+# HTTP only:
+# python3 -m http.server 8080
+
+# HTTPS
+# Ref: https://stackoverflow.com/a/56503260/5581893
+python3 <<HEREDOC
+from http.server import HTTPServer,SimpleHTTPRequestHandler 
+import ssl
+
+# Port: https://stackoverflow.com/a/32478278/5581893
+port=8443 
+httpd = HTTPServer(("localhost",port), SimpleHTTPRequestHandler)
+
+httpd.socket = ssl.wrap_socket(
+    httpd.socket,
+    keyfile     = "localhost.priv.pem",
+    certfile    = "localhost.cert.pem",
+    server_side = True
+)
+
+print(f"HTTPS serving at port {port}...")
+httpd.serve_forever()
+HEREDOC
 # EOF
