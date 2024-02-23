c31153b741f48764e2e5c6a8636388eeed2a6faf
Dan Deancook <dandeancook@tutanota.com>
2024-02-23 15:16:45 +0700

    Msg

--- wizard.sh
+++ wizard.sh
@@ -1,2 +1,13 @@
-echo "This wizard is a WIP"
+echo "1. Pack"
+echo "2. Pack Dev"
+read -p "Enter a choice (1..2): " Choice
+echo
+
+if [[ $Choice == 1 ]]; then
+    bash pack.sh
+elif [[ $Choice == 2 ]]; then
+    bash pack.sh dev
+else
+    echo "No such choice"
+fi
 # EOF
